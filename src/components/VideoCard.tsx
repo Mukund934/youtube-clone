0e6e29c761e11f2543edeabebf3f73f7ed61fd6b
Mukund934 <mukund.th04@gmail.com>
2025-03-08 13:21:44 +0530

    initial commit

---
+++ src/components/VideoCard.tsx
@@ -0,0 +1,27 @@
+export function VideoCard(props: any) {
+	return (
+		<div>
+			<img src={props.thumbnail} className="rounded-xl"></img>
+			<div className="grid grid-cols-12 pt-2">
+				<div className="col-span-1 pl-3 pt-1">
+					<img
+						className={"rounded-full w-12 h-12"}
+						src={props.channel}
+					></img>
+				</div>
+
+				<div className="col-span-11 pl-2">
+					<div>{props.title}</div>
+
+					<div className="col-span-11 text-base text-gray-400">
+						{props.author}
+					</div>
+
+					<div className="col-span-11 text-base text-gray-400">
+						{props.views} | {props.timeStamp}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
