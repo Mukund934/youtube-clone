056bdbedda6ed72d9387fc464d0873c803d9ebde
Mukund934 <mukund.th04@gmail.com>
2025-03-08 16:27:32 +0530

    fix VideoCard component, had problem in deploying

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -1,22 +1,36 @@
-export function VideoCard(props) {
+interface VideoCardProps {
+	thumbnail: string;
+	channel: string;
+	title: string;
+	author: string;
+	views: string;
+	timeStamp: string;
+}
+
+export function VideoCard({
+	thumbnail,
+	channel,
+	title,
+	author,
+	views,
+	timeStamp,
+}: VideoCardProps) {
 	return (
 		<div className="p-3 cursor-pointer">
-			<img src={props.thumbnail} className="rounded-xl" alt="thumbnail" />
+			<img src={thumbnail} className="rounded-xl" alt="thumbnail" />
 			<div className="grid grid-cols-12 pt-2">
 				<div className="col-span-1 pl-3 pt-1">
 					<img
 						className="rounded-full w-12 h-12"
-						src={props.channel}
+						src={channel}
 						alt="channel"
 					/>
 				</div>
 				<div className="col-span-11 pl-2">
-					<div>{props.title}</div>
+					<div>{title}</div>
+					<div className="text-base text-gray-400">{author}</div>
 					<div className="text-base text-gray-400">
-						{props.author}
-					</div>
-					<div className="text-base text-gray-400">
-						{props.views} | {props.timeStamp}
+						{views} | {timeStamp}
 					</div>
 				</div>
 			</div>
